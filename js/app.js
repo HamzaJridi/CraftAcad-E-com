d7f33cb572ad470aac331e8240b2de2de3fe7c0f
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-16 14:09:16 +0100

    add pnatl controller and displa articles

--- js/app.js
+++ js/app.js
@@ -2,19 +2,14 @@ var myApp = angular.module('myApp', ['ngRoute', 'pageControllers']);
 
 myApp.config(['$routeProvider', function($routeProvider){
     $routeProvider
-        .when('/pageOne', {
-            templateUrl : 'views/pageOne.html',
-            controller : 'PageOne'
-        })
-
         .when('/home', {
             templateUrl: 'views/home.html',
             controller: 'home'
         })
 
-        .when('/pageThree', {
-            templateUrl : 'views/pageThree.html',
-            controller : 'PageThree'
+        .when('/pantal', {
+            templateUrl : 'views/pantal.html',
+            controller : 'PantCtrl'
         })
 
         .otherwise({
--- js/controllers.js
+++ js/controllers.js
@@ -17,8 +17,8 @@ pageControllers.controller('home', ['$scope', '$http', function($scope, $http){
     });
 }]);
 
-pageControllers.controller('PageThree', ['$scope', '$http', function($scope, $http){
-    $http.get('js/pantal.json').success(function(_data){
-        $scope.data=_data;
+pageControllers.controller('PantCtrl', ['$scope', '$http', function($scope, $http){
+    $http.get('data/pantal.json').success(function(pantal){
+        $scope.pantal = pantal;
     });
 }]);
